1ea1afba0f67af87a70381bc3288a7cc36a6eb63
falexwolf <f.alex.wolf@gmx.de>
2021-11-11 21:19:46 +0100

    configure like scanpy

---
+++ conf.py
@@ -0,0 +1,46 @@
+from datetime import datetime
+project = 'anndata'
+author = 'andata developers'
+copyright = f'{datetime.now():%Y}, {author}'
+
+version = ''
+release = version
+
+extensions = [
+    'nbsphinx',
+]
+
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+language = None
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '**.ipynb_checkpoints']
+pygments_style = 'sphinx'
+
+# -- Options for HTML output ----------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = dict(
+    navigation_depth=4, logo_only=True  # Only show the logo
+)
+html_context = dict(
+    display_github=True,      # Integrate GitHub
+    github_user='theislab',   # Username
+    github_repo='anndata-tutorials',     # Repo name
+    github_version='master',  # Version
+    conf_py_path='/',    # Path in the checkout to the docs root
+)
+html_static_path = ['_static']
+html_show_sphinx = False
+
+def setup(app):
+    app.add_stylesheet('css/custom.css')
+
+# -- Strip output ----------------------------------------------
+
+import nbclean, glob
+
+for filename in glob.glob('**/*.ipynb', recursive=True):
+    ntbk = nbclean.NotebookCleaner(filename)
+    ntbk.clear('stderr')
+    ntbk.save(filename)
---
+++ index.rst
@@ -0,0 +1,13 @@
+.. include:: README.rst
+
+
+.. toctree::
+   :maxdepth: 1
+
+   pbmc3k
+   paga-paul15
+   plotting/core
+   integrating-data-using-ingest
+   spatial/basic-analysis
+   spatial/integration-scanorama
+..   cite-seq/pbmc5k
